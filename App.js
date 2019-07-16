b34e0262aa07aaa5702bca8cffd3bef625ce9897
simohutcaus <simonhutc@gmail.com>
2019-07-16 20:22:05 +1000

    first

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from 'react';
+import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import Routes from './screens/Routes'
+
+class App extends Component {
+ state = {
+   isFontLoaded: false
+ }
+
+ async componentDidMount() {
+   await Font.loadAsync({
+     'dogbyte': require('./assets/fonts/dogbyte.otf'),
+   });
+   this.setState({ isFontLoaded: true })
+ }
+
+ render() {
+   if(!this.state.isFontLoaded) {
+     return <AppLoading />
+   } else {
+     return (
+      <Fragment>
+        <StatusBar barStyle="light-content"></StatusBar>
+        <Routes />
+      </Fragment> 
+     )
+   }
+ }
+}
+
+export default App;
---
+++ screens/Routes.js
@@ -0,0 +1,17 @@
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Home from "./Home";
+
+
+const StackNavigator = createStackNavigator(
+ {
+   Home: {
+     screen: Home
+   }
+ },
+ {
+   initialRouteName: "Home",
+   headerMode: "none"
+ }
+);
+
+export default createAppContainer(StackNavigator);
